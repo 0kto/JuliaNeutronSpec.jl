ba09bc4f50f7ea752ce7b897a58749fa1d91bf7d
mjk <mjk@dmjk.net>
2020-11-11 01:08:19 +0100

    add tests for IN20
    Datafile taken from public Experiment ILL-Exp-4-01-1431 available under

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,13 @@ using JuliaNeutronSpec
 using Test
 
 @testset "Load Data" begin
-	dataPath = "test/fake_data"
-	for facility,instrument,pattern in [(:ILL, :IN8, "010101")]
-		myExperiment = Experiment("MySample","This is a sample experiment object", facility, "Test01", instrument, dataPath)
+	dataPath = "test/test_data"
+	for (facility,instrument,pattern) in [(:ILL, :IN20, "092575"),]
+		myExperiment = Experiment("MySample","This is a sample polarized TAS experiment", facility, "Test01", instrument, dataPath)
 		data_ill_in8 = load_data(myExperiment, pattern)
 
-		@test size(data_ill_in8) = (1,1)
+		# test if number of DataFrame columns is the same as the number
+		# of columns defined in the OrderedDict from columnsTAS().
+		@test size(data_ill_in8,1) = length(columnsTAS())
 	end
 end
