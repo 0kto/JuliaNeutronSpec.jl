5fbff157451bf0dd3238264467d1a0651ec7aa61
Maximilian Krautloher <mjk@dmjk.net>
2022-01-05 14:57:59 +0100

    Add histogram for DataFrames (up to 2D)

---
+++ src/optional-extensions/StatsBase.jl
@@ -0,0 +1,28 @@
+import StatsBase: fit
+function StatsBase.fit(::Type{Histogram},
+        df::AbstractDataFrame,
+        axes::Array{Symbol,1},
+        varargs...;
+        weights::Symbol=:NA,
+        kwargs... )
+    
+    print("hi")
+
+    if length(axes) == 1
+        data = convert(Array{Float64,1},df[axes[1]])
+    elseif length(axes) == 2
+        data = (convert(Array{Float64,1},df[axes[1]]),convert(Array{Float64,1},df[axes[2]]))
+    else
+        @error "at most two axes are supported."
+    end
+            
+    if weights != :NA
+        weights = Weights(Array{Float64,1}(df[weights]))
+        hist = StatsBase.fit(Histogram,data,weights,varargs... ;kwargs... )
+    else
+        hist = StatsBase.fit(Histogram,data,varargs... ;kwargs... )
+    end
+
+    return hist
+end
+export fit
