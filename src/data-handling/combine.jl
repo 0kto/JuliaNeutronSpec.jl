016199190ff0a229c6507984110708784795c803
Maximilian Krautloher <mjk@dmjk.net>
2022-01-06 12:58:18 +0100

    add data normalization

---
+++ src/data-handling/combine.jl
@@ -0,0 +1,21 @@
+function normalize!(
+    df::AbstractDataFrame;
+    monitor_col::Symbol=:MON,
+    monitor_count::Float64=1e4,
+    data_cols::Array{Symbol,1}=[:CNTS],
+)
+    ratio = df[monitor_col] ./ monitor_count
+
+    df[monitor_col] ./= ratio
+    for col = data_cols
+        df[col] ./= ratio
+    end
+
+    nothing
+end
+
+function normalize(df_in::AbstractDataFrame; kwargs...)
+    df = deepcopy(df_in)
+    normalize!(df; kwargs...)
+    return df
+end
