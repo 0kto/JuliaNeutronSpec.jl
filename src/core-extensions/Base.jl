d76d0a5085d420eeb67e61bc4167e5afba7aab95
Maximilian Krautloher <mjk@dmjk.net>
2022-01-13 20:57:07 +0100

    add / fix : rm bad argument, add methods for + & *

--- src/core-extensions/Base.jl
+++ src/core-extensions/Base.jl
@@ -33,20 +33,19 @@ function Base.:-(
             subtrahend,
             edges,
             bin_cols=[bin_col],
-            monitor_col=:MON,
             monitor_count=monitor_count
         )
     min = combine(
         minuend,
         edges,
         bin_cols=[bin_col],
-        monitor_col=:MON,
         monitor_count=monitor_count
     )
 
     # 2. subtraction.
-    names!(min, [ Symbol("min_$(col)") for col in names(min)])
-    res = innerjoin(sub, min, bin_col => Symbol("min_$(bin_col)") )
+    min = rename(min, [ Symbol("min_$(col)") for col in names(min)])
+
+    res = innerjoin(sub, min; on = bin_col => Symbol("min_$(bin_col)") )
     for col in  [ :CNTS, :SFx, :SFy, :SFz, :NSFx, :NSFy, :NSFz, :m_perp, :myy, :mzz ]
         if hasproperty(res, col)
             res[!,col] .-= res[:,Symbol("min_$(col)")]
@@ -57,6 +56,42 @@ function Base.:-(
     res[!,:time] .+= res[:,:min_time]
     res[!,:MON] .+= res[:,:min_MON]
 
-    return res[:,[names(sub)]]
+    return res
 end
 export -
+
+# import Base: +
+function Base.:+(
+    summand1::AbstractDataFrame,
+    summand2::AbstractDataFrame,
+    edges;
+    bin_col::Symbol=:EN,
+    monitor_count::Number=1e4
+)
+
+   for col in  [ :CNTS, :SFx, :SFy, :SFz, :NSFx, :NSFy, :NSFz, :m_perp, :myy, :mzz ]
+        if hasproperty(summand2, col)
+            summand2[!,col] .*= -1
+        end
+    end
+
+    return -(summand1, summand2, edges; bin_col=bin_col, monitor_count=monitor_count)
+end
+export +
+
+
+# import Base: *
+function Base.:*(
+    df_in::AbstractDataFrame,
+    factor
+)
+    df = deepcopy(df_in)
+    for col in  [ :MON, :CNTS, :SFx, :SFy, :SFz, :NSFx, :NSFy, :NSFz, :m_perp, :myy, :mzz ]
+        if hasproperty(df, col)
+            df[!,col] .*= factor
+        end
+    end
+
+    return df
+end
+export *
