70033a4ee1b9b7c5d9e8da9f6582e8de51b59896
Maximilian Krautloher <mjk@dmjk.net>
2022-01-13 19:12:49 +0100

    Add method to subtract DataFrames

--- src/core-extensions/Base.jl
+++ src/core-extensions/Base.jl
@@ -18,3 +18,45 @@ function Base.round(
     return df
 end
 export round
+
+# import Base: -
+function Base.:-(
+    subtrahend::AbstractDataFrame,
+    minuend::AbstractDataFrame,
+    edges;
+    bin_col::Symbol=:EN,
+    monitor_count::Number=1e4
+)
+
+    # 1. combine input.
+    sub = combine(
+            subtrahend,
+            edges,
+            bin_cols=[bin_col],
+            monitor_col=:MON,
+            monitor_count=monitor_count
+        )
+    min = combine(
+        minuend,
+        edges,
+        bin_cols=[bin_col],
+        monitor_col=:MON,
+        monitor_count=monitor_count
+    )
+
+    # 2. subtraction.
+    names!(min, [ Symbol("min_$(col)") for col in names(min)])
+    res = innerjoin(sub, min, bin_col => Symbol("min_$(bin_col)") )
+    for col in  [ :CNTS, :SFx, :SFy, :SFz, :NSFx, :NSFy, :NSFz, :m_perp, :myy, :mzz ]
+        if hasproperty(res, col)
+            res[!,col] .-= res[:,Symbol("min_$(col)")]
+        end
+    end
+    # 3. time / MON reflect the actual counting time / mon_ct that went into the 
+    #    measurment.
+    res[!,:time] .+= res[:,:min_time]
+    res[!,:MON] .+= res[:,:min_MON]
+
+    return res[:,[names(sub)]]
+end
+export -
